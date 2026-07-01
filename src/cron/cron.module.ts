f33807259d5ba3937e07da351af19af192b80a5f
Javier Montre Morales <129193992+jmontre@users.noreply.github.com>
2026-07-01 11:04:33 -0400

    feat[cron]: new cron to master with reminder (#12) (#13)
    Co-authored-by: javiermontre <javier.montre@evol.energy>

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChallengesCronService } from './challenges-cron.service';
+import { MasterCronService } from './master-cron.service';
 import { CronController } from './cron.controller';
 import { ChallengesModule } from '../challenges/challenges.module';
 
 @Module({
   imports: [ChallengesModule],
   controllers: [CronController],
-  providers: [ChallengesCronService],
+  providers: [ChallengesCronService, MasterCronService],
 })
 export class CronModule {}
