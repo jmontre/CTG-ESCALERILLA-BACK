8bc1b0adf999fa293808e99b1f838e828221b498
javiermontre <javier.montre@evol.energy>
2026-07-01 10:59:59 -0400

    feat[cron]: new cron to master with reminder

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
 import { ChallengesCronService } from './challenges-cron.service';
+import { MasterCronService } from './master-cron.service';
 import { Admin } from '../auth/admin.decorator';
 
 @Admin()
 @Controller('cron')
 export class CronController {
-  constructor(private cronService: ChallengesCronService) {}
+  constructor(
+    private cronService: ChallengesCronService,
+    private masterCronService: MasterCronService,
+  ) {}
 
   /**
    * POST /cron/run
@@ -14,6 +18,7 @@ export class CronController {
   @Post('run')
   async runCronManually() {
     await this.cronService.runManually();
+    await this.masterCronService.handleUnconfirmedMasterResults();
     return {
       message: 'Cron job ejecutado manualmente',
       timestamp: new Date(),
