6934106e60722da12ef12d6faea8d2d146a926bd
javiermontre <javier.montre@evol.energy>
2026-07-11 16:23:38 -0400

    feat: notificaciones in-app en eventos de reservas

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -3,10 +3,15 @@ import { JwtModule } from '@nestjs/jwt';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { jwtModuleOptions } from '../auth/jwt.config';
 
 @Module({
-  imports: [PrismaModule, JwtModule.registerAsync(jwtModuleOptions)],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync(jwtModuleOptions),
+    NotificationsModule,
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService],
   exports: [ReservationsService],
